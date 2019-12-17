b2d1fad7d79c8f9528b31b07e536af1dcf269049
Andrey Tchernov <hopeless.gm@yandex.ru>
2019-12-17 13:52:56 +0700

    #1 Fix icons, description, change-notes and bump version to milestone

--- src/main/kotlin/com/icerockdev/moko/intellij/MokoFeatureModule.kt
+++ src/main/kotlin/com/icerockdev/moko/intellij/MokoFeatureModule.kt
@@ -6,13 +6,18 @@ package com.icerockdev.moko.intellij
 
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.module.*
+import com.intellij.openapi.util.IconLoader
 import javax.swing.*
 
 class MokoFeatureModule: ModuleType<MokoFeatureBuilder>("moko_feature_module") {
     override fun createModuleBuilder(): MokoFeatureBuilder = MokoFeatureBuilder()
     override fun getName(): String = "Moko feature"
-    override fun getDescription(): String = "some desc"
+    override fun getDescription(): String = "Create feature sample with moko libraries"
     override fun getNodeIcon(isOpened: Boolean): Icon {
-        return AllIcons.General.Information
+        return IconLoader.getIcon("/icons/icon-16.svg")
+    }
+
+    override fun getIcon(): Icon {
+        return IconLoader.getIcon("/icons/icon-32.svg")
     }
 }
